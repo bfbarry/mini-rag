0f58a1e5459153674bc00707cbb85bf230db9627
Brian Barry <45830810+bfbarry@users.noreply.github.com>
2025-07-02 18:12:43 -0700

    checkpoint for rag

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@
 
 use ndarray::{Axis, Ix2};
 use ort::{
-    Error as OrtError,
-    session::{Session, builder::GraphOptimizationLevel},
-    value::TensorRef
+    session::{builder::GraphOptimizationLevel, Session}, value::{TensorRef}, Error as OrtError
 };
 use tokenizers::Tokenizer;
 use text_splitter::{TextSplitter, ChunkConfig};
 use std::{
-    error::Error, f32::NEG_INFINITY, fs
+    error::Error, fs
 };
 
 fn chunk_text(text: &str, chunk_size: usize, overlap: usize) -> Result<Vec<&str>, Box<dyn Error>> {
@@ -20,48 +18,82 @@ fn chunk_text(text: &str, chunk_size: usize, overlap: usize) -> Result<Vec<&str>
     Ok(chunks)
 }
 
+fn embed(chunks: &Vec<&str>, tokenizer: &Tokenizer, embedding_session: &mut Session) -> Result<ndarray::Array2<f32>, OrtError> {
+    let encodings = tokenizer.encode_batch(chunks.clone(), false)
+                    .map_err(|e| OrtError::new(e.to_string()))?;
 
-//https://github.com/pykeio/ort/blob/main/examples/sentence-transformers/semantic-similarity.rs
-fn main() -> ort::Result<()> {
-    let SAMPLE_INPUT  = fs::read_to_string("src/data/openapi_eda.json").expect("failed to read file");
-    let mut embedding_session = Session::builder()?
-        .with_optimization_level(GraphOptimizationLevel::Level1)?
-        .with_intra_threads(1)?
-        .commit_from_file("src/data/all-MiniLM-L6-v2.onnx")?;
+    let max_len = encodings.iter().map(|e| e.len()).max().unwrap();
+    if max_len > 512 {
+        return Err(OrtError::new("max_len must be < 512"));
+    }
+    let mut ids: Vec<i64> = Vec::new();
+    let mut mask: Vec<i64> = Vec::new();
 
-    let tokenizer = Tokenizer::from_file("src/data/tokenizer.json").unwrap();
-    
-    let chunk_size = 1000;
-    let inputs = chunk_text(&SAMPLE_INPUT, chunk_size, 200).unwrap();
+    // flattening token IDs and masks, while resizing
+    for encoding in &encodings {
+        let mut cur_ids :  Vec<i64> = encoding.get_ids().iter().map(|&id| id as i64).collect();
+        let mut cur_mask:  Vec<i64> = encoding.get_attention_mask().iter().map(|&m| m as i64).collect();
 
-    let encodings = tokenizer.encode_batch(inputs.clone(), false).map_err(|e| OrtError::new(e.to_string()))?;
+        cur_ids.resize(max_len, 0);
+        cur_mask.resize(max_len, 0);
 
-    
-    // flattening token IDs and masks
-    let ids: Vec<i64> = encodings.iter().flat_map(|e| e.get_ids().iter().map(|i| *i as i64)).collect();
-    let mask: Vec<i64> = encodings.iter().flat_map(|e| e.get_attention_mask().iter().map(|i| *i as i64)).collect();
-    
+        ids.extend(cur_ids);
+        mask.extend(cur_mask);
+    }
+
     // convert above into 2d tensors
-    let padded_token_length = chunk_size;
-    println!("{} {} {}", encodings[0].len(), encodings[1].len(), encodings[2].len());
-    let a_ids = TensorRef::from_array_view(([inputs.len(), padded_token_length], &*ids))?;
-    let a_mask = TensorRef::from_array_view(([inputs.len(), padded_token_length], &*mask))?;
+    let padded_token_length = max_len;
+    let shape = [chunks.len(), padded_token_length];
+
+    let a_ids   = TensorRef::from_array_view((shape, &*ids))?;
+    let a_mask  = TensorRef::from_array_view((shape, &*mask))?;
+
     // run model
     println!("here");
     let outputs = embedding_session.run(ort::inputs![a_ids, a_mask])?;
 
+    println!("jhere2");
     // get embeddings tensor (2d array)
     let embeddings = outputs[1].try_extract_array::<f32>()?.into_dimensionality::<Ix2>().unwrap();
     
-    println!("Sim for {}", inputs[0]);
-    let query = embeddings.index_axis(Axis(0), 0);
-    // let query = "create column";
-    println!("{:?}", query);
-    let min = NEG_INFINITY;
-    for (embeddings, sentence) in embeddings.axis_iter(Axis(0)).zip(inputs.iter()).skip(1) {
-        println!("HELPO");
-        let dot_product: f32 = query.iter().zip(embeddings.iter()).map(|(a,b)| a * b).sum();
-        println!("\t'{}': {:.1}%", sentence, dot_product*100.);
+    Ok(embeddings.into_owned()) 
+}
+
+//https://github.com/pykeio/ort/blob/main/examples/sentence-transformers/semantic-similarity.rs
+fn main() -> ort::Result<()> {
+    
+    let sample_input  = fs::read_to_string("src/data/openapi_eda.json").expect("failed to read file");
+    let mut embedding_session = Session::builder()?
+                                    .with_optimization_level(GraphOptimizationLevel::Level3)?
+                                    // .with_intra_threads(1)?
+                                    .commit_from_file("src/data/all-MiniLM-L6-v2.onnx")?;
+
+    let tokenizer = Tokenizer::from_file("src/data/tokenizer.json").unwrap();
+    
+    let chunk_size = 700;
+    let chunks = chunk_text(&sample_input, chunk_size, 200).unwrap();
+
+    let embeddings = embed(&chunks, &tokenizer, &mut embedding_session).unwrap();
+    
+    let query2 = "update retriever";
+    let query_embeddings = embed(&vec![query2], &tokenizer, &mut embedding_session).unwrap();
+    let query_vec = query_embeddings.index_axis(Axis(0), 0);
+    
+    // [3 1 2 4]
+    let mut max_cos = 0.;
+    let mut argmax = "";
+    let mut argmaxes: [&str; 3] = [""; 3];
+    let n1 = 1;
+    let n2 = 10;
+
+    for (embeddings, sentence) in embeddings.axis_iter(Axis(0)).zip(chunks.iter()) {
+        let dot_product: f32 = query_vec.iter().zip(embeddings.iter()).map(|(a,b)| a * b).sum();
+        if dot_product > max_cos {
+            max_cos = dot_product;
+            argmax = sentence;
+        }
+        // println!("\t'{}': {:.1}%", sentence, dot_product*100.);
     }
+    println!("Argmax is {}", argmax);
     Ok(())
 }
